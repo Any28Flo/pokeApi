83756adaac3395d39a294da6f96427cc2af9ea37
“Erika <foglzerika@gmail.com>
2017-06-13 07:54:25 -0500

    Agregando matelialize al diseño

--- js/app.js
+++ js/app.js
@@ -4,7 +4,28 @@ var cargaPagina = function(){
 
 
 }
-
+var arregloImagenes = [
+	{"src":"img/1bulbasaur.png"},
+	{"src":"img/2ivysaur.png"},
+	{"src":"img/3venusaur.png"},
+	{"src":"img/4charmander.png"},
+	{"src":"img/5charmeleon.png"},
+	{"src":"img/6charizard.png"},
+	{"src":"img/7squirtle.png"},
+	{"src":"img/8Wartortle.png"},
+	{"src":"img/9Blastoise.png"},
+	{"src":"img/10Caterpie.png"},
+	{"src":"img/11Metapod.png"},
+	{"src":"img/12Butterfree.png"},
+	{"src":"img/13Weedle.png"},
+	{"src":"img/14Kakuna.png"},
+	{"src":"img/15Beedrill.png"},
+	{"src":"img/16Pidgey.png"},
+	{"src":"img/17Pidgeotto.png"},
+	{"url":"img/18Pitget.png"},
+	{"src":"img/19Rattata.png"},
+	{"src":"img/20Raticate.png"}
+	]
 var listaPokemon = function (){
 	
 	var url = "http://pokeapi.co/api/v2/pokemon-species/"
@@ -29,12 +50,23 @@ var muestraTotal = function (numeroPokemon){
 var muestraPokemones = function(listaPokemones){
 	var $lista = $("#listaPokemon");
 
-	listaPokemones.forEach(function(pokemon){
+	listaPokemones.forEach(function(pokemon,posicion){
+		var ruta= arregloImagenes[posicion].src;
+
+		console.log(arregloImagenes[posicion]);
+		var $foto = $("<img/>");
+		var $boton = $("<a>Ver poquemon </a>");
+		$foto.attr("src",ruta);
 		var $itemPokemon = $("<li/>");
-		$itemPokemon.addClass("pokemon");
+		$itemPokemon.addClass("pokemon card col s10 l4");
+		$boton.addClass("card-action");
+		$foto.addClass("card-image responsive-img");
 		$itemPokemon.text(pokemon.name);
 		$itemPokemon.attr("data-url",pokemon.url);
+
 		$lista.append($itemPokemon);
+		$itemPokemon.append($foto);
+		$itemPokemon.append($boton);
 
 	})
 }
